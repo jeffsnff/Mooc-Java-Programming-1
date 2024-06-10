db4490875daa6da6f21dc7dbeaf62eda7ac37eae
Jeffrey Seneff <jeffsnff@gmail.com>
2024-06-10 06:47:01 -0600

    Copleted Part 1 : Reading Input

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -7,6 +7,14 @@ public class Conversation {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+      String usersDay;
+      String usersReply;
+      System.out.println("Greetings! How are you doing?");
+      usersDay = scanner.nextLine();
+      System.out.println("Oh, how interesting. Tell me more!");
+      usersReply = scanner.nextLine();
+      scanner.close();
+      System.out.println("Thanks for sharing!");
 
     }
 }
--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -7,6 +7,17 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        String name;
+        String career;
 
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        name = scanner.nextLine();
+        System.out.println("What is their job?");
+        career = scanner.nextLine();
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was "+ name +", who was "+ career +".");
+        System.out.println("On the way to work, "+ name +" reflected on life.");
+        System.out.println("Perhaps "+ name +" will not be "+ career +" forever.");
     }
 }
