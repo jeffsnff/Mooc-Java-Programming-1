0975e86375786edc824080db1ead15a99ff33889
Jeffrey Seneff <jeffsnff@gmail.com>
2024-06-10 06:23:57 -0600

    Pushing all Mooc - Java code to github

---
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+        // Create a String variable and store users input in variable when read by scanner
+        String userInput = scanner.nextLine();
+        // Print out users input
+        System.out.println(userInput);
+
+    }
+}
