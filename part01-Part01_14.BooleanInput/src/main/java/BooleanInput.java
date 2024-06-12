0404f39f83e3e358230fb2a409cc82355cd5a69a
Jeffrey Seneff <jeffsnff@gmail.com>
2024-06-12 06:11:20 -0600

    more java mooc exercises

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -5,8 +5,10 @@ public class BooleanInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        Boolean userInput;
         // write your program here
-
+        System.out.println("Write something:");
+        userInput = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? "+userInput);
     }
 }
